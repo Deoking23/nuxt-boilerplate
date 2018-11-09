2e3689f60d71213ec90172e6a8a37dc8510501fa
deokin <bowom90@aartkorea.com>
2018-11-09 18:02:06 +0900

    추가

--- meta.js
+++ meta.js
@@ -21,8 +21,8 @@ module.exports = {
       'message': 'Author'
     },
     skipInterpolation: [
-      "pages/**",
-      "static/fonts/**"
+      "template/pages/**",
+      "template/static/fonts/**"
     ]
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
